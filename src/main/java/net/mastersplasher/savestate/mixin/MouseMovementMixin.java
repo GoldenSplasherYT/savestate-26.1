ca4b56356af99b42ecf1dbb21c6d8bd9ef86a259
masterwyatt <masterwyatt05@gmail.com>
2026-03-29 14:28:02 -0500

    Fixes the accumulated mouse velocity staying when unfrozen

--- src/main/java/net/mastersplasher/savestate/mixin/MouseMovementMixin.java
+++ src/main/java/net/mastersplasher/savestate/mixin/MouseMovementMixin.java
@@ -1,26 +1,27 @@
 package net.mastersplasher.savestate.mixin;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.mastersplasher.savestate.SavestateClient;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
 public class MouseMovementMixin {
-    @Inject(method = "turnPlayer", at = @At("HEAD"), cancellable = true)
-    private void turnPlayer(double mousea, CallbackInfo ci) {
-        if (SavestateClient.isFrozen) {
-            ci.cancel();
-        }
-    }
 
-    @Inject(method = "handleAccumulatedMovement", at = @At("HEAD"), cancellable = true)
-    private void handleAccumulatedMovement(CallbackInfo ci) {
+    @Shadow
+    private double accumulatedDX;
+
+    @Shadow
+    private double accumulatedDY;
+
+    @Inject(method = "turnPlayer", at = @At("HEAD"), cancellable = true)
+    private void onTurnPlayer(double mousea, CallbackInfo ci) {
         if (SavestateClient.isFrozen) {
+            accumulatedDX = 0;
+            accumulatedDY = 0;
             ci.cancel();
         }
     }
