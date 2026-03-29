65e41ea233252f05e6b2431dc5654515aa1c23c7
Opjeet Dhanoa <569039nkc@gmail.com>
2026-03-28 21:14:09 -0500

    new

--- src/main/java/net/mastersplasher/savestate/SavestateClient.java
+++ src/main/java/net/mastersplasher/savestate/SavestateClient.java
@@ -10,6 +10,8 @@ import org.lwjgl.glfw.GLFW;
 
 public class SavestateClient implements ClientModInitializer {
 
+    boolean isFrozen = false;
+
     KeyMapping.Category CATEGORY = new KeyMapping.Category(
             Identifier.fromNamespaceAndPath(Savestate.MOD_ID, "savestate_keybinds")
     );
@@ -22,12 +24,64 @@ public class SavestateClient implements ClientModInitializer {
                     CATEGORY
             ));
 
+    KeyMapping toggleFreezeKey = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.savestate.toggle_freeze",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_F6,
+                    CATEGORY
+            ));
+
+    KeyMapping savestateKey = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.savestate.savestate",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_F7,
+                    CATEGORY
+            ));
+
+    KeyMapping loadstateKey = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.savestate.loadstate",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_F8,
+                    CATEGORY
+            ));
+
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (sendToChatKey.consumeClick()) {
                 if (client.player != null) {
                     client.player.sendSystemMessage(Component.literal("Key was pressed!"));
+                }
+            }
+
+            while (toggleFreezeKey.consumeClick()) {
+                if (client.player != null) {
+                    if (!isFrozen) {
+                        isFrozen = true;
+                        client.player.sendSystemMessage(Component.literal("Game is Frozen!"));
+                    } else {
+                        isFrozen = false;
+                        client.player.sendSystemMessage(Component.literal("Game is not Frozen!"));
+                    }
+                }
+            }
+
+            while (savestateKey.consumeClick()) {
+                if (client.player != null) {
+                    if (isFrozen) {
+                        client.player.sendSystemMessage(Component.literal("Savestate saved"));
+                    } else {
+                        client.player.sendSystemMessage(Component.literal("Game is not Frozen"));
+                    }
+                }
+            }
+
+            while (loadstateKey.consumeClick()) {
+                if (client.player != null) {
+                    client.player.sendSystemMessage(Component.literal("Savestate loaded"));
                 }
             }
         });
