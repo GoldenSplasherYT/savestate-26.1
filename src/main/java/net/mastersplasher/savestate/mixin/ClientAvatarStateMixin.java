d21482b53971c6d8e14982f838acea9a0943f548
masterwyatt <masterwyatt05@gmail.com>
2026-03-29 15:36:49 -0500

    did a different way of saving the player limbs

---
+++ src/main/java/net/mastersplasher/savestate/mixin/ClientAvatarStateMixin.java
@@ -0,0 +1,9 @@
+package net.mastersplasher.savestate.mixin;
+
+import net.minecraft.client.entity.ClientAvatarState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientAvatarState.class)
+public class ClientAvatarStateMixin {
+
+}
--- src/main/java/net/mastersplasher/savestate/mixin/PlayerModelMixin.java
+++ src/main/java/net/mastersplasher/savestate/mixin/PlayerModelMixin.java
@@ -1,16 +1,18 @@
 package net.mastersplasher.savestate.mixin;
 
 import net.mastersplasher.savestate.SavestateClient;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.client.model.player.PlayerModel;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Player.class)
-public class PlayerMixin {
-    @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
-    private void tick(CallbackInfo ci) {
+@Mixin(PlayerModel.class)
+public class PlayerModelMixin {
+
+    @Inject(method = "setupAnim(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;)V", at = @At("HEAD"), cancellable = true)
+    private void setupAnim(AvatarRenderState state, CallbackInfo ci) {
         if (SavestateClient.isFrozen) {
             ci.cancel();
         }
