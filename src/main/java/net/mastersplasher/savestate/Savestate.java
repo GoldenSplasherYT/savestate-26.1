0a2ccf4e57a818fc675e1a97265bd68b9a6977b7
masterwyatt <masterwyatt05@gmail.com>
2026-03-29 20:28:26 -0500

    h4xx0r3d by Andrew Lemmingson!!!11!

--- src/main/java/net/mastersplasher/savestate/Savestate.java
+++ src/main/java/net/mastersplasher/savestate/Savestate.java
@@ -1,13 +1,9 @@
 package net.mastersplasher.savestate;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.mastersplasher.savestate.Payload.LoadPayload;
 import net.mastersplasher.savestate.Payload.PausePayload;
-import net.mastersplasher.savestate.Payload.SavePayload;
-import net.minecraft.server.level.ServerPlayer;
 import org.joml.Vector3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,29 +19,11 @@ public class Savestate implements ModInitializer {
 		AtomicReference<Vector3d> pos = null;
 
 		PayloadTypeRegistry.serverboundPlay().register(PausePayload.ID, PausePayload.CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(SavePayload.ID, SavePayload.CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(LoadPayload.ID, LoadPayload.CODEC);
 
 		ServerPlayNetworking.registerGlobalReceiver(PausePayload.ID, ((payload, context) -> {
 			context.server().execute(() -> {
 				var tickManager = context.server().tickRateManager();
 				tickManager.setFrozen(SavestateClient.isFrozen);
-			});
-		}));
-
-		ServerPlayNetworking.registerGlobalReceiver(SavePayload.ID, ((payload, context) -> {
-			context.server().execute(() -> {
-				ServerPlayer player = context.player();
-
-				pos.set(new Vector3d(player.getX(), player.getY(), player.getZ()));
-			});
-		}));
-
-		ServerPlayNetworking.registerGlobalReceiver(LoadPayload.ID, ((payload, context) -> {
-			context.server().execute(() -> {
-				ServerPlayer player = context.player();
-
-				player.setPos(pos.get().x, pos.get().y, pos.get().z);
 			});
 		}));
 	}
