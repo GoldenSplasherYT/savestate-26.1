d86306a3c305cef0c7dd1d25ac23cd4ecf556575
Opjeet Dhanoa <569039nkc@gmail.com>
2026-04-06 09:51:35 -0500

    Created Logic For Saving World (Entity Saving Logic is written but is buggy at this moment)

--- src/main/java/net/mastersplasher/savestate/Savestate.java
+++ src/main/java/net/mastersplasher/savestate/Savestate.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Savestate implements ModInitializer {
@@ -53,13 +54,116 @@ public class Savestate implements ModInitializer {
 			saveState.putDouble("playerZ", player.getZ());
 			saveState.putFloat("playerYaw", player.getYRot());
 			saveState.putFloat("playerPitch", player.getXRot());
+
+			int radius = 50;
+			BlockPos center = player.blockPosition();
+			BlockPos min = new BlockPos(center.getX() - radius, center.getY() - radius, center.getZ() - radius);
+			BlockPos max = new BlockPos(center.getX() + radius, center.getY() + radius, center.getZ() + radius);
+
+			saveState.putInt("anchorX", min.getX());
+			saveState.putInt("anchorY", min.getY());
+			saveState.putInt("anchorZ", min.getZ());
+			saveState.putInt("radius", radius);
+
+			int size = (radius * 2 + 1);
+			int[] blockData = new int[size * size * size];
+			int index = 0;
+
+			for (int y = min.getY(); y <= max.getY(); y++) {
+				for (int z = min.getZ(); z <= max.getZ(); z++) {
+					for (int x = min.getX(); x <= max.getX(); x++) {
+						BlockState state = player.level().getBlockState(new BlockPos(x, y, z));
+						blockData[index++] = Block.getId(state);
+					}
+				}
+			}
+			saveState.putIntArray("BlockData", blockData);
+
+			ListTag entityList = new ListTag();
+			ServerLevel world = player.level();
+			int entityRadius = 64;
+
+			for (Entity entity : world.getAllEntities()) {
+				if (entity instanceof Player) {
+					continue;
+				}
+
+				if (entity.distanceToSqr(player) < (entityRadius * entityRadius)) {
+					TagValueOutput output = TagValueOutput.createWithContext(ProblemReporter.DISCARDING, world.registryAccess());
+					entity.saveWithoutId(output);
+
+					entityList.add(output.buildResult());
+				}
+			}
+			saveState.put("Entities", entityList);
+
+//            try {
+//                save(server);
+//            } catch (IOException e) {
+//				LOGGER.error("Error Running Save Method: ", e);
+//            }
         })));
 
 		ServerPlayNetworking.registerGlobalReceiver(LoadPayload.ID, ((payload, context) -> context.server().execute(() -> {
 			var server = context.server();
 			ServerPlayer player = server.getPlayerList().getPlayers().getFirst();
 
-			player.teleportTo(saveState.getDoubleOr("x", player.getX()), saveState.getDoubleOr("y", player.getY()), saveState.getDoubleOr("z", player.getZ()));
+			player.teleportTo(
+					player.level(),
+					saveState.getDouble("playerX").get(),
+					saveState.getDouble("playerY").get(),
+					saveState.getDouble("playerZ").get(),
+					java.util.Set.of(),
+					saveState.getFloat("playerYaw").get(),
+					saveState.getFloat("playerPitch").get(),
+					true
+			);
+
+			int[] blockData = saveState.getIntArray("BlockData").get();
+			int anchorX = saveState.getIntOr("anchorX", 0);
+			int anchorY = saveState.getIntOr("anchorY", 0);
+			int anchorZ = saveState.getIntOr("anchorZ", 0);
+			int radius = saveState.getIntOr("radius", 0);
+
+			int size = (radius * 2 + 1);
+			int index = 0;
+
+			for (int y = 0; y < size; y++) {
+				for (int z = 0; z < size; z++) {
+					for (int x = 0; x < size; x++) {
+						BlockPos pos = new BlockPos(anchorX + x, anchorY + y, anchorZ + z);
+						BlockState savedState = Block.stateById(blockData[index++]);
+
+						player.level().setBlock(pos, savedState, 2 | 16);
+					}
+				}
+			}
+
+			ServerLevel world = player.level();
+			int entityRadius = 64;
+
+			ListTag entityList = saveState.getListOrEmpty("Entities");
+
+			for (int i = 0; i < entityList.size(); i++) {
+				CompoundTag entityData = (CompoundTag) entityList.get(i);
+
+				TagValueInput input = (TagValueInput) TagValueInput.create(ProblemReporter.DISCARDING, world.registryAccess(), entityData);
+
+				EntityType.loadEntityRecursive(entityData, world, EntitySpawnReason.LOAD, (loadedEntity) -> {
+					loadedEntity.load(input);
+
+					world.addFreshEntity(loadedEntity);
+					return loadedEntity;
+				});
+			}
+
+			for (Entity entity : world.getAllEntities()) {
+				if (!(entity instanceof Player) && entity.distanceToSqr(player) < (entityRadius * entityRadius)) {
+					entity.discard(); // Remove them from the world instantly
+				}
+			}
+
+//			loadSave(server);
 		})));
 	}
 }
